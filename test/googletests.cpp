464e353561e75861fe8d2c179e77cd84581f8d74
Jere <jere.luhtanen@gmail.com>
2022-10-20 22:24:42 +0300

    Added some basic methods

--- test/googletests.cpp
+++ test/googletests.cpp
@@ -1,7 +1,28 @@
 #include <gtest/gtest.h>
 #include "../RingBuffer.hpp"
 #include "../TestClass.hpp"
+#include <string>
+#include <vector>
 
-TEST(mainframe, TestBufferContainer) {
-    
+TEST(mainframe, defaultConstruction){
+
+    RingBuffer<int> control();
+    EXPECT_EQ(control.empty(), true);
+
+}
+
+TEST(mainframe, copyConstruction){
+
+    RingBuffer<int> control();
+    RingBuffer<int> experiment(control);
+
+    control.resize();
+    EXPECT_EQ()
+    EXPECT_EQ(experiment.size(), control.size());
+    EXPECT_EQ(sizeof(experiment), sizeof(control));
+}
+
+    //EXPECT_EQ(experiment.end(), control.end());
+    //EXPECT_EQ(experiment.begin(),control.begin());
+
 }
